869fa47322307e6d06514eaafd2e7452ba45264c
Tayyaba Rasheed <kaneezamna234@gmail.com>
2024-12-15 16:00:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+//1//
+
+let multiArray = [[], [], []];
+
+// //2 //
+// let matrix = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+//   ];
+  
+// document.write("<h2> Matrix:</h2>")
+// document.write(matrix[0],"<br>");
+// document.write(matrix[1],"<br>");
+// document.write(matrix[2],"<br>");
+
+ // 3
+ 
+ 
+// document.write("Counting from 1 to 10: <br>");
+// for (let i = 1; i <= 10; i++) {
+//   document.write(i ,"<br>");
+// }
+
+// 4 
+
+// let table = prompt("enter a number");
+// let length = prompt("enter a length of table");
+
+// document.write("multiplication table of" , table, "length is" , length , "<br>");
+// for(let i=1;i<=length;i++){
+    
+//         document.write(  "<br>",table +"*"+i + "=" , table*i);
+        
+    // }
+ // 5//
+
+ let fruits=["apple","banana","mango","orange","strawberry"];
+ document.write(fruits , "<br>");
+for(let i=0;i<fruits.length;i++){
+    document.write( "<br>" ,"the element at  index" + i + " is "+fruits[i]);
+}
+
+// 6 
+
